13261a7cb7cb41e068da4269a157fdbb283d288f
Ivan <ivan.bondarenko.it@gmail.com>
2023-01-26 20:06:08 +0400

    preparing for deployment

--- checkout/forms.py
+++ checkout/forms.py
@@ -8,13 +8,13 @@ class OrderCreateForm(forms.ModelForm):
         model = Order
         fields = ['first_name', 'last_name', 'email', 'phone', 'city', 'address', 'comment']
 
-    phone_validator = RegexValidator(regex=r'^+?3?8?0?\d{2}[- ]?(\d[- ]?){7}$',
-                                     message='Phone number incorrect')
+    # phone_validator = RegexValidator(regex=r'^+?3?8?0?\d{2}[- ]?(\d[- ]?){7}$',
+    #                                  message='Phone number incorrect')
 
     first_name = forms.CharField(widget=forms.TextInput())
     last_name = forms.CharField(widget=forms.TextInput())
     email = forms.EmailField(widget=forms.EmailInput())
-    phone = forms.CharField(widget=forms.TextInput(), validators=[phone_validator])
+    phone = forms.CharField(widget=forms.TextInput())
     city = forms.CharField(widget=forms.TextInput())
     address = forms.CharField(widget=forms.TextInput())
     comment = forms.CharField(widget=forms.TextInput())
