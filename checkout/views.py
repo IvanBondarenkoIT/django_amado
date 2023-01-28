300bf49751d9b4492009e4ce5e4d8267317a236a
Ivan <ivan.bondarenko.it@gmail.com>
2023-01-28 15:09:32 +0400

    Deploy

--- checkout/views.py
+++ checkout/views.py
@@ -15,9 +15,9 @@ def order_create(request):
             for item in cart:
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
                                          quantity=item['quantity'])
-                #clear the cart
-                cart.clear()
-                return render(request, 'created.html', {'order': order})
+            #clear the cart
+            cart.clear()
+            return render(request, 'created.html', {'order': order})
     else:
         form = OrderCreateForm()
     return render(request, 'checkout.html', {'cart': cart, 'form': form})
